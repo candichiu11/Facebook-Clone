dd5497fc0a8022d6cdae84d2eb121dbf8cf67c25
Candi Lilun Chiu <candi.chiu@egym.com>
2024-03-19 21:33:21 +0100

    finished the Frontend

--- src/Feed.js
+++ src/Feed.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import "./Feed.css"
 import StoryReel from "./StoryReel"
+import MessageSender from './MessageSender'
+import Post from './Post'
+import candi from "./images/Candi.jpeg"
 
 function Feed() {
   return (
     <div className='feed'>
     <StoryReel />
-    {/* StoryReel */}
-    {/* MessageSender */}
-      
+    <MessageSender />
+    <Post 
+        profilePic={candi}
+        message="WOW it's amazing"
+        image={"https://picsum.photos/500/300"}
+        timestamp={"This is a timestamp"}
+        username={"cchiu"}
+    />
+    <Post />
+    <Post />
     </div>
   )
 }
